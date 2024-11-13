d1b83608aa56519e1b8fcb6fa23b9a3dfeda9091
Hayes Chiasson <hchias2@lsu.edu>
2024-11-13 16:46:08 -0600

    started on business sectin of home page

--- frontend/Suni/src/BusinessPopUp.tsx
+++ frontend/Suni/src/BusinessPopUp.tsx
@@ -1,12 +1,10 @@
-
-
-function QuizPopUp() {
+function BusinessPopUp() {
     return (
       <>
         <div className=" ml-4 max-w-md mx-auto bg-blue-100/80 rounded-3xl shadow-md overflow-hidden md:max-w-2xl ju=">
               {/*image location*/}
-              <div className="quiz-pop-up-title text-5xl my-10 flex justify-center font-extrabold">
-                Take the Quiz!
+              <div className="quiz-pop-up-title text-4xl my-10 flex justify-center font-extrabold">
+                Apply your Business!
               </div>
               <div className="flex justify-center font-bold">Let Suni know your Preferences, and he'll plan your trip for you!</div>
             <div className="p-10 text-center space-x-7">
@@ -28,18 +26,4 @@ function QuizPopUp() {
     );
   }
 
-  export default QuizPopUp
-
-// <div className="ml-4 max-w-md mx-auto bg-blue-100/80 rounded-3xl shadow-md overflow-hidden md:max-w-2xl ju=">
-//       {/*image location*/}
-//       <div className="text-5xl my-10 mx-40 font-extrabold ">
-//         Take the Quiz!
-//       </div>
-//     </div>
-//     <div className="p-10 text-center">
-//       <div >Test</div>
-//       <a href="#">Button 1</a>
-//         <button className="quiz-pop-up-button ">Surprise Me</button>
-//         <button className="quiz-pop-up-button">Help me Decide</button>
-//         <button className="quiz-pop-up-button">Let me Pick</button>
-// </div>
+  export default BusinessPopUp
--- frontend/Suni/src/pages/Home.tsx
+++ frontend/Suni/src/pages/Home.tsx
@@ -1,42 +1,79 @@
 //import PopOut from '../PopOut.tsx' //to get imports/files from src, use ../
+import BusinessPopUp from '../BusinessPopUp.tsx';
 import QuizPopUp from '../QuizPopUp.tsx'
 
 
 function Home() {
     return (
-<section>
-  <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
-    <div className="max-w-3xl">
-      <h2 className="text-5xl font-extrabold sm:text-4xl">
-        Tell us about your dream trip!
-      </h2>
-    </div>
+      <>
+        <section>
+          <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
+            <div className="max-w-3xl">
+              <h2 className="text-5xl font-extrabold sm:text-4xl">
+                Tell us about your dream trip!
+              </h2>
+            </div>
 
-    <div className="mt-8 grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
-      <div className="relative h-64 overflow-hidden sm:h-80 lg:h-full mt-10">
-        <QuizPopUp/>
-      </div>
+            <div className="mt-8 grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
+              <div className="relative h-64 overflow-hidden sm:h-80 lg:h-full mt-10">
+                <QuizPopUp/>
+              </div>
 
-      <div className="lg:py-16">
-        <article className="space-y-4 text-gray-600">
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut qui hic atque tenetur quis
-            eius quos ea neque sunt, accusantium soluta minus veniam tempora deserunt? Molestiae
-            eius quidem quam repellat.
-          </p>
+              <div className="lg:py-16">
+                <article className="space-y-4 text-gray-600">
+                  <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut qui hic atque tenetur quis
+                    eius quos ea neque sunt, accusantium soluta minus veniam tempora deserunt? Molestiae
+                    eius quidem quam repellat.
+                  </p>
 
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum explicabo quidem
-            voluptatum voluptas illo accusantium ipsam quis, vel mollitia? Vel provident culpa
-            dignissimos possimus, perferendis consectetur odit accusantium dolorem amet voluptates
-            aliquid, ducimus tempore incidunt quas. Veritatis molestias tempora distinctio
-            voluptates sint! Itaque quasi corrupti, sequi quo odit illum impedit!
-          </p>
-        </article>
-      </div>
-    </div>
-  </div>
-</section>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum explicabo quidem
+                    voluptatum voluptas illo accusantium ipsam quis, vel mollitia? Vel provident culpa
+                    dignissimos possimus, perferendis consectetur odit accusantium dolorem amet voluptates
+                    aliquid, ducimus tempore incidunt quas. Veritatis molestias tempora distinctio
+                    voluptates sint! Itaque quasi corrupti, sequi quo odit illum impedit!
+                  </p>
+                </article>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section>
+          <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
+            <div className="max-w-3xl">
+              <h2 className="text-5xl font-extrabold sm:text-4xl">
+                Apply your Business
+              </h2>
+            </div>
+
+            <div className="mt-8 grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
+              <div className="relative h-64 overflow-hidden sm:h-80 lg:h-full mt-10">
+                <BusinessPopUp/>
+              </div>
+
+              <div className="lg:py-16">
+                <article className="space-y-4 text-gray-600">
+                  <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut qui hic atque tenetur quis
+                    eius quos ea neque sunt, accusantium soluta minus veniam tempora deserunt? Molestiae
+                    eius quidem quam repellat.
+                  </p>
+
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum explicabo quidem
+                    voluptatum voluptas illo accusantium ipsam quis, vel mollitia? Vel provident culpa
+                    dignissimos possimus, perferendis consectetur odit accusantium dolorem amet voluptates
+                    aliquid, ducimus tempore incidunt quas. Veritatis molestias tempora distinctio
+                    voluptates sint! Itaque quasi corrupti, sequi quo odit illum impedit!
+                  </p>
+                </article>
+              </div>
+            </div>
+          </div>
+        </section>
+    </>
     );
   }
 
