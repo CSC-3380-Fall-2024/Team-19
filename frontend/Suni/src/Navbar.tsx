e30d9a53641aa87850b617a22f3f9458ae1d959a
Hayes Chiasson <hayeschiasson1@yahoo.com>
2024-12-05 23:18:23 -0600

    fixed coloring

--- frontend/Suni/src/Navbar.tsx
+++ frontend/Suni/src/Navbar.tsx
@@ -84,7 +84,7 @@ const Navbar: React.FC = () => {
                     <button className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 "
                       onClick={signOut}
                       >
-                      Sign out
+                      Sign Out
                     </button>
                   </li>
                 </ul>
@@ -93,7 +93,7 @@ const Navbar: React.FC = () => {
             </div>
           ) : (
             <>
-              <Link to="/accountPage" className="text-white bg-drk-txt-blue hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-6 py-2 text-center">
+              <Link to="/accountPage" className="text-white bg-drk-blue hover:bg-drk-2-blue focus:ring-1 focus:outline-none focus:ring-drk-2-blue font-medium rounded-lg text-sm px-6 py-2 text-center">
                 Log in
               </Link>
             </>
@@ -113,19 +113,19 @@ const Navbar: React.FC = () => {
           </button>
         </div>
         <div className={`items-center justify-between ${isMobileMenuOpen ? '' : 'hidden'} w-full md:flex md:w-auto md:order-1`} id="navbar-user">
-          <ul className="bg-sky-blue flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:sky-blue"> {/*Border box for main links(both in main and mini menu*/}
+          <ul className="bg-sky-blue flex flex-col font-medium p-4 md:p-0 mt-4 border rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:sky-blue"> {/*Border box for main links(both in main and mini menu*/}
             <li>
-              <Link to="/quizPage" className="block py-2 px-3 text-drk-blue rounded hover:bg-blue-100 md:hover:bg-transparent md:hover:text-btn-blue md:p-0 ">
+              <Link to="/quizPage" className="block py-2 px-3 text-drk-blue rounded hover:bg-blue-100 md:hover:bg-transparent md:hover:text-drk-2-blue md:p-0 ">
                 Quiz
               </Link>
             </li>
             <li>
-              <Link to="./calendarPage" className="block py-2 px-3 text-drk-blue rounded hover:bg-blue-100 md:hover:bg-transparent md:hover:text-btn-blue md:p-0 ">
+              <Link to="./calendarPage" className="block py-2 px-3 text-drk-blue rounded hover:bg-blue-100 md:hover:bg-transparent md:hover:text-drk-2-blue md:p-0 ">
                 Calendar
               </Link>
             </li>
             <li>
-              <Link to="/businessAppPage" className="block py-2 px-3 text-drk-blue rounded hover:bg-blue-100 md:hover:bg-transparent md:hover:text-btn-blue md:p-0 ">
+              <Link to="/businessAppPage" className="block py-2 px-3 text-drk-blue rounded hover:bg-blue-100 md:hover:bg-transparent md:hover:text-drk-2-blue md:p-0 ">
                 BusinessApp
               </Link>
             </li>
